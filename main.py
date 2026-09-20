0002732835e2057f20bb5a9f36c6c12402e7fbd6
Eric Fernandes <Ericfernandes958@gmail.com>
2026-09-20 10:35:47 +0100

    Added pie chart to display results

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 rng = np.random.default_rng()
 numbers = np.array([1, 2, 3, 4, 5, 6])
 
-data = rng.choice(numbers, 10000)
+rolls = rng.choice(numbers, 10000)
+
+colours = ["red", "blue", "green", "purple", "pink", "orange"]
+
+frequency = np.bincount(rolls)[1:]
+
+plt.pie(frequency, labels=numbers, autopct="%1.1f%%", colors=colours)
+
+plt.title("Dice roll distribution")
+
+plt.show()
