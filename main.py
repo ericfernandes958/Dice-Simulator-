13d6208b916bb2509ac3b18712be9e058e1d6b1e
Eric Fernandes <Ericfernandes958@gmail.com>
2026-09-23 13:51:52 +0100

    Added graph to show how the expected and actual results compare when varying the sample size

--- main.py
+++ main.py
@@ -13,12 +13,28 @@ sample_sizes = [10, 100, 1_000, 10_000, 100_000]
 results = {}
 colours = ["red", "blue", "green", "purple", "pink", "orange"]
 
+# Results for pie and grouped bar chart
+
 for size in sample_sizes:
     rolls = rng.choice(numbers, size)
     frequency = np.bincount(rolls, minlength=7)[1:]
     results[size] = frequency
 
-figure, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+# Error calculations
+
+errors = {}
+
+for size in sample_sizes:
+    frequency = results[size]
+
+    actual_Percetage = (frequency / size) * 100
+    expected_Percentage = 100 / 6
+    percentage_Error = np.abs(actual_Percetage - expected_Percentage)
+
+    errors[size] = np.mean(percentage_Error)
+
+
+figure, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
 
 
 def update_graph(sample_size):
@@ -40,7 +56,6 @@ def update_graph(sample_size):
     # Bar graph
 
     ax2.barh(numbers - width / 2, [expected] * 6, height=width, label="Expected")
-
     ax2.barh(numbers + width / 2, frequency, height=width, label="Actual")
 
     ax2.set_title(f"Expected vs Actual ({sample_size:,} rolls)")
@@ -54,10 +69,17 @@ def update_graph(sample_size):
 # Radio buttons
 
 radio_ax = figure.add_axes([0.02, 0.3, 0.12, 0.4])
-
 radio = RadioButtons(radio_ax, [str(size) for size in sample_sizes])
-
 radio.on_clicked(lambda label: update_graph(int(label)))
+
+ax3.plot(sample_sizes, [errors[size] for size in sample_sizes], marker="o")
+
+ax3.set_title("How does Sample Size affect Average Error?")
+ax3.set_xlabel("No of rolls")
+ax3.set_ylabel("Average Percentage error")
+ax3.set_xscale("log")
+ax3.grid(True)
+
 
 # Initial graph
 
